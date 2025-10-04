9165a61d07d221485daeccf6aab80be8de76e90d
Yash708262 <yash708262@gmail.com>
2025-10-04 19:51:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function onesDigit(n) {
-  // your code here
+  // Convert input to a number
+  const num = Number(n);
+
+  // Validate input: must be a positive integer
+  if (!Number.isInteger(num) || num <= 0) {
+    throw new Error("Input must be a positive integer");
+  }
+
+  // Return the ones digit
+  return num % 10;
 }
 
 // Do not change the code below
-
 const n = prompt("Enter Number:");
 alert(onesDigit(n));
+
